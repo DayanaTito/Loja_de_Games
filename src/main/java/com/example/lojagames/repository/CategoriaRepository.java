a96c0b9eee30fcfe87b7fbecea562f0d18aaaaaa
Dayana Tito <Dayanatitochoque@hotmail.com>
2022-05-04 12:34:49 -0300

    file upload

--- src/main/java/com/example/lojagames/repository/CategoriaRepository.java
+++ src/main/java/com/example/lojagames/repository/CategoriaRepository.java
@@ -3,9 +3,11 @@ package com.example.lojagames.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.lojagames.model.Categoria;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 	
 	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
