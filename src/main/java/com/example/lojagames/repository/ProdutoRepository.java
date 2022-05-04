02945c9d1162c44dc673bad903be7ea81e8a7086
Dayana Tito <Dayanatitochoque@hotmail.com>
2022-05-04 12:21:00 -0300

    file upload

---
+++ src/main/java/com/example/lojagames/repository/ProdutoRepository.java
@@ -0,0 +1,17 @@
+package com.example.lojagames.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.lojagames.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+
+	public List <Produto> findByPrecoGreaterThanOrderByPreco(BigDecimal preco);
+	
+	public List <Produto> findByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
+}
