21e0e1b3145127db180deccc7c623a2bc3e43806
Dayana Tito <Dayanatitochoque@hotmail.com>
2022-05-11 14:19:38 -0300

    Spring Security

---
+++ src/main/java/com/example/lojagames/controller/UsuarioController.java
@@ -0,0 +1,83 @@
+package com.example.lojagames.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lojagames.model.Usuario;
+import com.example.lojagames.model.UsuarioLogin;
+import com.example.lojagames.repository.UsuarioRepository;
+import com.example.lojagames.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping ("/all") // método de listar todos os usuarios
+	public ResponseEntity <List<Usuario>> getAll(){
+		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+	
+	@GetMapping("/{id}") // método de buscar por id
+	public ResponseEntity<Usuario> buscarporid(@PathVariable Long id){
+		return usuarioRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping("/logar") // método logar
+	public ResponseEntity<UsuarioLogin> loginUsuario(@RequestBody Optional <UsuarioLogin> usuarioLogin){
+		
+		return usuarioService.autenticarUsuario(usuarioLogin)
+			.map(respostaLogin -> ResponseEntity.status(HttpStatus.OK).body(respostaLogin))
+			.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PostMapping("/cadastrar") // método para cadastrar usuario
+	public ResponseEntity<Usuario> postUsuario(@Valid @RequestBody Usuario usuario){
+		
+		return usuarioService.cadastrarUsuario(usuario)
+			.map(respostaCadastrar -> ResponseEntity.status(HttpStatus.CREATED).body(respostaCadastrar))
+			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+	}
+	
+	@PutMapping("/atualizar")  // método de atualizar usuario
+	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario) {
+		return usuarioService.atualizarUsuario(usuario)
+			.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+	}
+	
+	@DeleteMapping("/delete/{id}")// metedo de deletar por id
+	public ResponseEntity<?> deletePostagem(@PathVariable Long id) {
+		
+		return usuarioRepository.findById(id)
+			.map(resposta -> {
+				usuarioRepository.deleteById(id);
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+			})
+			.orElse(ResponseEntity.notFound().build());
+	}
+
+}
--- src/main/java/com/example/lojagames/model/Produto.java
+++ src/main/java/com/example/lojagames/model/Produto.java
@@ -45,7 +45,12 @@ public class Produto {
 	@ManyToOne
 	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
-
+	
+	/*@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;*/
+	
+	
 	public Long getId() {
 		return id;
 	}
